86bab95531ad064074c607b4a53d152d0159a368
gsoykan <gurkan@naylalabs.com>
2019-06-21 00:57:43 +0300

    Initial Commit

---
+++ MovieDBNowPlaying/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieDBNowPlaying
+//
+//  Created by gurkan soykan on 21.06.2019.
+//  Copyright © 2019 gurkan soykan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
